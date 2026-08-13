5b1772cbf27ce60bfc92b15f491bb38cfc9420a9
chun-mingpangext-coder <chun-ming.pang.ext@fashion-digital.de>
2026-08-13 16:36:56 +0200

    refactor: remove redundant column aliases in staging CDP dbt models

--- src/dapp/dbt/models/staging/cdp/staging__cdp__activities.sql
+++ src/dapp/dbt/models/staging/cdp/staging__cdp__activities.sql
@@ -6,18 +6,18 @@
 
 SELECT
   id AS activity_id,
-  activity_type AS activity_type,
-  source AS source,
-  source_id AS source_id,
-  person_id AS person_id,
-  company_id AS company_id,
-  title AS title,
-  activity_date AS activity_date,
-  summary_or_content AS summary_or_content,
-  to_dos AS to_dos,
-  participants AS participants,
-  url AS url,
-  metadata AS metadata,
-  created_at AS created_at,
-  updated_at AS updated_at
+  activity_type,
+  source,
+  source_id,
+  person_id,
+  company_id,
+  title,
+  activity_date,
+  summary_or_content,
+  to_dos,
+  participants,
+  url,
+  metadata,
+  created_at,
+  updated_at
 FROM {{ source('s_cdp', 'activities') }}
